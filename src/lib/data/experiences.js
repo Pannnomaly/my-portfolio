8f7045a31a2409c6f45b21458eef3326f7ffddb7
Pannnomaly <supawith007@gmail.com>
2026-03-02 20:36:07 +0700

    feat(work): implement work section with experience and education details

--- src/lib/data/experiences.js
+++ src/lib/data/experiences.js
@@ -1,23 +1,9 @@
 export const experiences = [
     {
         id: 1,
-        title: "title1",
-        subTitle: "sub-title1",
-        year: "year1",
-        description: "description1",
-    },
-    {
-        id: 2,
-        title: "title2",
-        subTitle: "sub-title2",
-        year: "year2",
-        description: "description2",
-    },
-    {
-        id: 3,
-        title: "title3",
-        subTitle: "sub-title3",
-        year: "year3",
-        description: "description3",
+        title: "Pakchong Dairy Cooperative",
+        subTitle: "Nakhon Ratchasima",
+        year: "2023",
+        description: "At Pakchong Dairy Cooperative, I performed structured data analysis on raw milk quality metrics, applying analytical reasoning and rigorous documentation practices aligned with technical problem-solving processes. By evaluating three years of operational data, I identified performance trends and quality insights, reinforcing my ability to interpret complex datasets and contribute effectively within collaborative, data-driven workflows.",
     },
 ];
