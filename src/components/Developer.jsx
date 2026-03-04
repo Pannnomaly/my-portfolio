6de6f150e26e617cc4bb319ae46ae7e4ecb715d3
Pannnomaly <supawith007@gmail.com>
2026-03-04 23:40:35 +0700

    feat(services): implement framer-motion to developer section

--- src/components/Developer.jsx
+++ src/components/Developer.jsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import { develops } from "../lib/data/develops.js";
 
 export default function Developer ()
 {
+    const [open, setOpen] = useState(false);
+
     return (
         <div className="relative bg-(--container-color) py-8 px-6 rounded-[3rem] grid gap-y-6 overflow-hidden">
             <div className="blob-animate-service"></div>
@@ -13,20 +17,38 @@ export default function Developer ()
                 </p>
             </div>
 
-            <div className="relative z-1">
-                <h3 className="text-(--text-color) font-normal text-[1.25rem] md:text-[1.5rem] mb-2">Skills & Tools</h3>
+            <AnimatePresence>
+                {open && (
+                    <motion.div
+                        initial={{ opacity: 0, height: 0 }}
+                        animate={{ opacity: 1, height: "auto" }}
+                        exit={{ opacity: 0, height: 0 }}
+                        transition={{ duration: 0.4 }}
+                        className="relative z-1 overflow-hidden"
+                    >
+                        <h3 className="text-(--text-color) font-normal text-[1.25rem] md:text-[1.5rem] mb-2">Skills & Tools</h3>
+                    
+                        <ul className="flex flex-wrap items-center gap-y-2 gap-x-2">
+                            {develops.map((develop) => (
+                                    <li key={develop.id} id="service-skill-lists" className="text-[1rem] md:text-[1.25rem] bg-(--container-color) text-(--title-color) py-1 px-2.5 rounded-[4rem] font-unbounded">{develop.name}</li>
+                            ))}
+                        </ul>
+                    </motion.div>
+                )}
+            </AnimatePresence>
 
-                <ul className="flex flex-wrap items-center gap-y-2 gap-x-2">
-                    {develops.map((develop) => (
-                        <div key={develop.id}>
-                            <li id="service-skill-lists" className="text-[1rem] md:text-[1.25rem] bg-(--container-color) text-(--title-color) py-1 px-2.5 rounded-[4rem] font-unbounded">{develop.name}</li>
-                        </div>
-                    ))}
-                </ul>
-            </div>
-
-            <button className="grid place-items-center w-9 h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 shrink-0 rounded-lg bg-(--body-color) absolute right-8 bottom-6 lg:bottom-4 z-2 cursor-pointer">
-                <svg id="service-button" className="w-9 h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 shrink-0 text-(--title-color) rounded-lg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"></path></svg>
+            <button onClick={() => setOpen(!open)} className="grid place-items-center w-9 h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 shrink-0 rounded-lg bg-(--body-color) absolute right-8 bottom-6 lg:bottom-4 z-2 cursor-pointer">
+                <motion.svg
+                    id="service-button"
+                    animate={{ rotate: open ? 180 : 0 }}
+                    transition={{ duration: 0.3 }}
+                    className="w-9 h-9 md:w-10 md:h-10 lg:w-11 lg:h-11 text-(--title-color) rounded-lg"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                >
+                    <path d="M11.9999 13.1714L16.9497 8.22168L18.3639 9.63589L11.9999 15.9999L5.63599 9.63589L7.0502 8.22168L11.9999 13.1714Z"/>
+                </motion.svg>
             </button>
         </div>
     );
