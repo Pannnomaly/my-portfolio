20ba27cbfd2a391bc173ec76ea77a35cdbbf6b5b
Pannnomaly <supawith007@gmail.com>
2026-03-09 01:19:48 +0700

    feat(contact): implement react-copy-to-clipboard to contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,11 @@
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { useState } from "react";
 import Container from "../lib/Container.jsx";
 
 export default function Contact ()
 {
+    const [copied, setCopied] = useState(false);
+
     return (
         <div className="pt-20 xl:min-h-screen lg:pt-36">
             <Container>
@@ -13,11 +17,12 @@ export default function Contact ()
                             Tell me about your next project.
                         </p>
 
-                        <div id="button-cv" className="cursor-pointer bg-(--body-color) text-(--title-color) py-4 px-8 rounded-[4rem] font-unbounded inline-flex items-center gap-x-2">
-                            <span className="text-[1.25rem] md:text-[1.5rem] lg:text-[1.75rem] transition duration-400 ease-in-out">Copy Email</span>
-                            <span><svg className="w-6 h-6 md:w-7 md:h-7 lg:w-9 lg:h-9 shrink-0 font-normal transition duration-400 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6.9998 6V3C6.9998 2.44772 7.44752 2 7.9998 2H19.9998C20.5521 2 20.9998 2.44772 20.9998 3V17C20.9998 17.5523 20.5521 18 19.9998 18H16.9998V20.9991C16.9998 21.5519 16.5499 22 15.993 22H4.00666C3.45059 22 3 21.5554 3 20.9991L3.0026 7.00087C3.0027 6.44811 3.45264 6 4.00942 6H6.9998ZM5.00242 8L5.00019 20H14.9998V8H5.00242ZM8.9998 6H16.9998V16H18.9998V4H8.9998V6Z"></path></svg></span>
-                            <span className="hidden">s.jangtrakul@gmail.com</span>
-                        </div>
+                        <CopyToClipboard text="s.jangtrakul@gmail.com" onCopy={() => {setCopied(true); setTimeout(() => setCopied(false), 2000);}}>
+                            <button id="button-cv" className="cursor-pointer bg-(--body-color) text-(--title-color) py-4 px-8 rounded-[4rem] font-unbounded inline-flex items-center gap-x-2">
+                                <span className="text-[1.25rem] md:text-[1.5rem] lg:text-[1.75rem] transition duration-400 ease-in-out">{copied ? "Email copied" : "Copy Email"}</span>
+                                <span>{copied ? (<svg className="w-6 h-6 md:w-7 md:h-7 lg:w-9 lg:h-9 shrink-0 font-normal transition duration-400 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M9.9997 15.1709L19.1921 5.97852L20.6063 7.39273L9.9997 17.9993L3.63574 11.6354L5.04996 10.2212L9.9997 15.1709Z"></path></svg>) : (<svg className="w-6 h-6 md:w-7 md:h-7 lg:w-9 lg:h-9 shrink-0 font-normal transition duration-400 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M6.9998 6V3C6.9998 2.44772 7.44752 2 7.9998 2H19.9998C20.5521 2 20.9998 2.44772 20.9998 3V17C20.9998 17.5523 20.5521 18 19.9998 18H16.9998V20.9991C16.9998 21.5519 16.5499 22 15.993 22H4.00666C3.45059 22 3 21.5554 3 20.9991L3.0026 7.00087C3.0027 6.44811 3.45264 6 4.00942 6H6.9998ZM5.00242 8L5.00019 20H14.9998V8H5.00242ZM8.9998 6H16.9998V16H18.9998V4H8.9998V6Z"></path></svg>)}</span>
+                            </button>
+                        </CopyToClipboard>
                     </div>
 
                     <div id="contact-card" className="grid gap-y-12">
