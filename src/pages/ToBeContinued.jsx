bbf9365876e680035d96b8d5f396160facc0cf92
Pannnomaly <supawith007@gmail.com>
2026-03-09 01:51:50 +0700

    style(error): styling error page and implement to be continued page

--- src/pages/ToBeContinued.jsx
+++ src/pages/ToBeContinued.jsx
@@ -5,7 +5,7 @@ export default function Error ()
             <div id="blob-animate-about" className="top-10 left-10"></div>
             <div id="blob-animate-about" className="right-10 bottom-10 md:bottom-18 lg:-right-18 lg:bottom-16 xl:right-20"></div>
             
-            <h1 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[3.25rem] text-shadow-lg"><span className="text-(--first-color)">404</span> - Page not found!</h1>
+            <h1 className="text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[3.25rem] text-shadow-lg">To be <span className="text-(--first-color)">continued. . .</span></h1>
         </div>
     );
 }
