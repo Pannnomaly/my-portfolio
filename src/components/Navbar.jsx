28301ff3ce4802119ae07aac12d9d3dfd2c5c1da
Pannnomaly <supawith007@gmail.com>
2026-03-09 20:51:04 +0700

    feat(scrollbar): add highlight to navbar for scrolling behavior

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,68 @@
+import { useEffect } from "react";
 import Container from "../lib/Container.jsx";
 
-export default function Navbar() {
+export default function Navbar() 
+{
+    useEffect(() => {
+
+    const sectionIds = [
+    "hero-section",
+    "about-me-section",
+    "projects-section",
+    "contact-section"
+    ];
+
+    const sections = sectionIds.map(id => document.getElementById(id));
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    const observer = new IntersectionObserver((entries) => {
+
+        entries.forEach((entry) => {
+
+        if (entry.isIntersecting) {
+
+            navLinks.forEach((link) => {
+            link.classList.remove("active");
+            });
+
+            const activeLink = document.querySelector(
+            `.nav-link[href="#${entry.target.id}"]`
+            );
+
+            activeLink?.classList.add("active");
+
+        }
+
+        });
+
+    },
+    {
+        threshold: 0.6
+    });
+
+    sections.forEach((section) => observer.observe(section));
+
+    return () => observer.disconnect();
+
+    }, []);
+
     return (
         <nav className="fixed w-full py-2 pt-5 bg-linear-to-b from-(--body-color) from-60% to-[hsla(240,100%,2%,0)] z-(--z-fixed)">
             <div id="blob-animate" className="-top-12 -left-12"></div>
 
             <Container className="flex justify-between items-center py-4 font-unbounded tracking-widest">
                 <div className="font-semibold text-(--title-color) text-shadow-2xs hover:text-(--first-color) self-start transition duration-400 ease-in-out">
-                    <a href="#hero-section">Supawith</a>
+                    <a href="#hero-section" className="nav-link">Supawith</a>
                 </div>
                 <ul className="flex flex-col items-end gap-y-2 md:flex-row md:gap-x-12 lg:gap-x-22">
                     <li className="text-(--title-color) hover:text-(--first-color) font-semibold">
-                        <a href="#about-me-section" className="text-(--small-font-size) text-shadow-2xs transition duration-400 ease-in-out">About Me</a>
+                        <a href="#about-me-section" className="nav-link text-(--small-font-size) text-shadow-2xs transition duration-400 ease-in-out">About Me</a>
                     </li>
                     <li className="text-(--title-color) hover:text-(--first-color) font-semibold">
-                        <a href="#projects-section" className="text-(--small-font-size) text-shadow-2xs transition duration-400 ease-in-out">Projects</a>
+                        <a href="#projects-section" className="nav-link text-(--small-font-size) text-shadow-2xs transition duration-400 ease-in-out">Projects</a>
                     </li>
                     <li className="text-(--title-color) hover:text-(--first-color) font-semibold">
-                        <a href="#contact-section" className="text-(--small-font-size) text-shadow-2xs transition duration-400 ease-in-out">Contact</a>
+                        <a href="#contact-section" className="nav-link text-(--small-font-size) text-shadow-2xs transition duration-400 ease-in-out">Contact</a>
                     </li>
                 </ul>
             </Container>
