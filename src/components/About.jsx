614c36842f5c88a173d3c1bc7a92d978a591b2ae
Pannnomaly <supawith007@gmail.com>
2026-03-10 02:17:10 +0700

    feat(animation): implement scroll animation to all project component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+import { aboutText, aboutImage } from "../lib/animations/aboutReveal.js";
 import Container from "../lib/Container.jsx";
 
 export default function About ()
@@ -7,7 +9,7 @@ export default function About ()
             <div id="shadow-about" className="w-full h-64.5 absolute -bottom-1 md:bottom-24 lg:h-150 z-1"></div>
             <Container id="about-container" className="relative pt-16">
                 <div className="grid gap-y-12 xl:grid-cols-2 xl:items-center xl:gap-x-20">
-                    <div className="md:text-center xl:text-start xl:z-2">
+                    <motion.div {...aboutText} className="md:text-center xl:text-start xl:z-2">
                         <h2 className="mb-6 text-start md:text-center xl:text-start xl:z-2 text-[2rem] md:text-[2.5rem] lg:text-[3rem] xl:text-[3.25rem]">
                             <span className="text-(--first-color)">Where Creativity</span>
                             <br />Meets Code
@@ -19,14 +21,14 @@ export default function About ()
                             <a className="text-[1.25rem] md:text-[1.5rem] transition duration-400 ease-in-out" href="/pdf/supawithCV.pdf" download target="_blank">Resume</a>
                             <span><svg className="w-6 h-6 md:w-7 md:h-7 lg:w-9 lg:h-9 shrink-0 font-normal transition duration-400 ease-in-out" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M21 8V20.9932C21 21.5501 20.5552 22 20.0066 22H3.9934C3.44495 22 3 21.556 3 21.0082V2.9918C3 2.45531 3.4487 2 4.00221 2H14.9968L21 8ZM19 9H14V4H5V20H19V9ZM8 7H11V9H8V7ZM8 11H16V13H8V11ZM8 15H16V17H8V15Z"></path></svg></span>
                         </div>
-                    </div>
-                    <div className="relative justify-self-center xl:-order-1">
+                    </motion.div>
+                    <motion.div {...aboutImage} className="relative justify-self-center xl:-order-1">
                         <div id="blob-animate-about" className="top-5 -left-8"></div>
                         <div id="blob-animate-about" className="-right-8 bottom-10 md:bottom-18 lg:-right-18 lg:bottom-16 xl:right-6"></div>
                         <div className="flex justify-center mt-12 xl:pr-24">
                             <img className="w-60 md:w-80 lg:w-110 xl:w-120 self-center" src="profile.PNG" alt="profile picture" />
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
             </Container>
         </div>
